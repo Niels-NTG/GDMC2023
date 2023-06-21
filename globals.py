5b3bedac8c507c7c042d168b77cd9ceb82ea36a0
Niels NTG <ntg999@gmail.com>
2023-06-21 17:54:06 +0200

    Restrict worldSlice size to a maximum size of 656x656

--- globals.py
+++ globals.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 
+import glm
+
 from gdpc.gdpc import interface
 from gdpc.gdpc import Editor
+from gdpc.gdpc.vector_tools import Rect
 from StructureFolder import StructureFolder
 
 global structureFolders
@@ -18,7 +21,9 @@ def initialize():
     loadStructureFiles()
 
     global buildarea
+    maxBuildAreaSize = 656
     buildarea = interface.getBuildArea().toRect()
+    buildarea = buildarea.centeredSubRect(glm.min(buildarea.size, glm.ivec2(maxBuildAreaSize, maxBuildAreaSize)))
     global editor
     editor = Editor()
     editor.loadWorldSlice(rect=buildarea, cache=True)
